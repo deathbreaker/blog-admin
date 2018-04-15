756be08cc035ac83122c59933d9e7f3cdc2a91cd
deathbreaker <oristgrey@gmail.com>
2018-04-15 06:52:37 +0200

    Without .idea

--- index.php
+++ index.php
@@ -2,21 +2,20 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-/*use Symfony\Component\Debug\Debug;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\Debug\ExceptionHandler;
 use Symfony\Component\Debug\DebugClassLoader;
 use Symfony\Component\Debug\ErrorHandler;
+
 Debug::enable();
 ExceptionHandler::register();
 DebugClassLoader::enable();
-ErrorHandler::register();*/
+ErrorHandler::register();
 
 $router = new Gears\Router();
 $router->routesPath = 'routes/routes.php';
 $router->dispatch();
 
-use Tracy\Debugger;
-Debugger::enable();
 
 
 
